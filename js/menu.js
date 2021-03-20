ec6fe66383744e43a73feeea1daaaee728a67ff5
Luis-Salinas-SD <paus.impala21@gmail.com>
2021-03-19 21:41:57 -0600

    Actualización al menu purpura

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,8 @@
+//aparece y desaparece el menu al tocar el icono de hamburguesa
 const hamburger = document.querySelector('.menu-icon');
-const nav_menu = document.querySelector('.nav-bar')
+const nav_menu = document.querySelector('.menu')
 
 hamburger.addEventListener('click', () => {
     nav_menu.classList.toggle("spread")
     console.log('algo');
 })
-
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
-}
